dab331cd15b4689b967c732f82c69e41610ed165
Coray Bennett <cob3218@rit.edu>
2022-12-30 22:48:47 -0500

    initial project creation

---
+++ metroroulette/src/main/java/com/cob3218/metroroulette/MetrorouletteApplication.java
@@ -0,0 +1,13 @@
+package com.cob3218.metroroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrorouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetrorouletteApplication.class, args);
+	}
+
+}
---
+++ metroroulette/src/test/java/com/cob3218/metroroulette/MetrorouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cob3218.metroroulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetrorouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
