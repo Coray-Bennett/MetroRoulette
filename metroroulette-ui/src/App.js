8880d557a2ac713f9a8c78bd3d9c76ace1eea8e9
Coray Bennett (RIT Student) <cob3218@rit.edu>
2023-04-09 00:43:02 -0400

    title animation

--- metroroulette-ui/src/App.js
+++ metroroulette-ui/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="title">Metro Roulette</h1>
+      <h1 className="title noselect">METRO ROULETTE</h1>
     </div>
   );
 }
