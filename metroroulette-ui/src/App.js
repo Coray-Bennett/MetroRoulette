f56f8166b93e821fd19cacd860b8ecfcda586f17
Coray Bennett (RIT Student) <cob3218@rit.edu>
2023-04-09 12:19:39 -0400

    info icon, more styling

--- metroroulette-ui/src/App.js
+++ metroroulette-ui/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Input from './Input.js';
+import Info from './Info';
 
 const App = () => {
   return (
     <div className="App">
       <h1 className="title noselect">METRO ROULETTE</h1>
       <Input />
+      <Info />
     </div>
   );
 }
---
+++ metroroulette-ui/src/Info.js
@@ -0,0 +1,15 @@
+import './Info.css';
+
+const Info = () => {
+
+
+    return (
+        <>
+        <button className="info">
+            <span className="mif-info"></span>
+        </button>
+        </>
+    )
+}
+
+export default Info;
